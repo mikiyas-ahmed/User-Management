6b69026d9768aeda45754407abea21b0868ab80a
Mikiyas Eshetu <mikiyasahmed7@gmail.com>
2022-02-21 23:47:19 +0100

    finalizing for deployment

--- src/test/java/com/carparking/UserManagement/dao/UserRepositoryIntegrationTest.java
+++ src/test/java/com/carparking/UserManagement/dao/UserRepositoryIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.carparking.dao;
+package com.carparking.UserManagement.dao;
 
 
 import static org.junit.Assert.assertEquals;
@@ -7,17 +7,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.carparking.UserManagement.UserManagementApplication;
-import com.carparking.UserManagement.dao.UserRepository;
 import com.carparking.UserManagement.entity.User;
 
-@ContextConfiguration(classes=UserManagementApplication.class)
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @SqlGroup(@Sql( executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts= {"classpath:schema.sql","classpath:data.sql"}))
